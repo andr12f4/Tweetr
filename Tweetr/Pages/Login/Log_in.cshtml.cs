00f95d34c6daf2d50b34e9138cb911fb9997b700
AlexTriumf <TriumfAlex@gmail.com>
2021-03-10 12:24:38 +0100

    igen

--- Tweetr/Pages/Login/Log_in.cshtml.cs
+++ Tweetr/Pages/Login/Log_in.cshtml.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Tweetr.Models;
 
 namespace Tweetr.Pages.Login
 {
     public class Log_inModel : PageModel
     {
+        [BindProperty]
+        public Customer Customer { get; set; }
+        [BindProperty]
+        public string Username { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+
         public void OnGet()
         {
+
+        }
+
+        public IActionResult OnPost()
+        {
+
+            CustomerHandler customerHandler = new CustomerHandler();
+            Customer = customerHandler.Get(Username, Password);
+            if (Customer != null)
+            {
+                HttpContext.Session.SetString("user", JsonConvert.SerializeObject(Customer));
+                return RedirectToPage("/Index");
+            }
+            if (!ModelState.IsValid) { return Page(); }
+            return Page();
         }
     }
 }
--- Tweetr/Pages/Login/OpretBruger.cshtml.cs
+++ Tweetr/Pages/Login/OpretBruger.cshtml.cs
@@ -2,15 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Tweetr.Models;
 
 namespace Tweetr.Pages.Login
 {
     public class OpretBrugerModel : PageModel
     {
+        public Customer CustomerSession { get; set; }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
         public void OnGet()
         {
+            try
+            {
+                CustomerSession = JsonConvert.DeserializeObject<Customer>(HttpContext.Session.GetString("user"));
+            }
+            catch (ArgumentNullException ex) { }
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+
+                Customer.CustomerDiscount = true;
+                CustomerHandler cH = new CustomerHandler();
+                cH.Create(Customer);
+
+                return RedirectToPage("/Index");
+            }
+            return Page();
+
         }
     }
 }
